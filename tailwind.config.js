6c9455abd641fe29898e54632823e840e662afd3
gitweb2025-dot <gitweb2025@gmail.com>
2025-08-20 10:41:33 +0530

    12th

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"], // adjust to match your files
+  content: [".html"], // adjust to match your files
   darkMode: "class", // ✅ important
   theme: {
     extend: {},
